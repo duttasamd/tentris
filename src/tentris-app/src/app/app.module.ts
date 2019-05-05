6c6ae07971c2c2d4ca039d1db382d4c141431ffa
Samrat Dutta <dutta.sam.d@gmail.com>
2019-05-05 21:58:40 +0200

    [UIB-01-02] Generated Components : navbar, menusidebar, editor, viewer.

--- src/tentris-app/src/app/app.module.ts
+++ src/tentris-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MenusidebarComponent } from './components/menusidebar/menusidebar.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { ViewerComponent } from './components/viewer/viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MenusidebarComponent,
+    EditorComponent,
+    ViewerComponent
   ],
   imports: [
     BrowserModule,
