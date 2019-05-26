5e93f5550d6da46c1044de6cbb20c66bc2e5809d
Samrat Dutta <dutta.sam.d@gmail.com>
2019-05-27 00:22:36 +0200

    [UB-02-03] Syntax Highlighting [UB-02-05] Code Completion [UB-02-06] Auto Formatting
    Added Ace.js; Cleanup Needed. Follow trello ub0203 comment for now.

--- src/tentris-app/src/app/components/editor/editor.component.ts
+++ src/tentris-app/src/app/components/editor/editor.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as ace from 'ace-builds';
+
+// language package, choose your own
+import 'ace-builds/src-noconflict/mode-sparql';
+// ui-theme package
+import 'ace-builds/src-noconflict/theme-textmate';
+
+import 'ace-builds/src-noconflict/ext-language_tools';
+
+import 'ace-builds/src-noconflict/ext-beautify';
+
+const THEME = 'ace/theme/textmate';
+const LANG = 'ace/mode/sparql';
 
 @Component({
   selector: 'app-editor',
@@ -7,9 +20,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
+  @ViewChild('codeEditor') codeEditorElmRef: ElementRef;
+  private codeEditor: ace.Ace.Editor;
+  private editorBeautify;
+
   constructor() { }
 
   ngOnInit() {
+    ace.require('ace/ext/language_tools');
+
+    const element = this.codeEditorElmRef.nativeElement;
+    const editorOptions = this.getEditorOptions();
+
+    this.editorBeautify = ace.require('ace/ext/beautify');
+    this.codeEditor = ace.edit(element, editorOptions);
+    this.codeEditor.setTheme(THEME);
+    this.codeEditor.getSession().setMode(LANG);
+    this.codeEditor.setShowFoldWidgets(true);
+  }
+
+  private getEditorOptions(): Partial<ace.Ace.EditorOptions> & { enableBasicAutocompletion?: boolean; } {
+    const basicEditorOptions: Partial<ace.Ace.EditorOptions> = {
+      highlightActiveLine: true,
+      minLines: 14,
+      maxLines: 22,
+      autoScrollEditorIntoView:true
+    };
+
+    const extraEditorOptions = {
+      enableBasicAutocompletion: true
+    };
+    const margedOptions = Object.assign(basicEditorOptions, extraEditorOptions);
+    return margedOptions;
+  }
+
+  public beautifyContent() {
+    if (this.codeEditor && this.editorBeautify) {
+      const session = this.codeEditor.getSession();
+      this.editorBeautify.beautify(session);
+    }
+  }
+
+  private getCode() {
+    const code = this.codeEditor.getValue();
+    console.log(code);
   }
 
 }
