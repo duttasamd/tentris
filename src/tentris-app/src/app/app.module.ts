a76d4270d7cd2a19f3ea0a503cc22753854d1b03
Jalaj Bajpai <bajpaijalaj@gmail.com>
2019-05-26 23:59:26 +0200

    [UIB-03-07] Added sorting logic to results table

--- src/tentris-app/src/app/app.module.ts
+++ src/tentris-app/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { MenusidebarComponent } from './components/menusidebar/menusidebar.compo
 import { EditorComponent } from './components/editor/editor.component';
 import { ViewerComponent } from './components/viewer/viewer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule, MatTabsModule, MatButtonModule, MatListModule, MatPaginatorModule } from '@angular/material';
+import { MatTableModule, MatTabsModule, MatButtonModule, MatListModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -26,7 +26,8 @@ import { MatTableModule, MatTabsModule, MatButtonModule, MatListModule, MatPagin
     MatTabsModule,
     MatButtonModule,
     MatListModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
